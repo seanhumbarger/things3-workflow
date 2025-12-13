ce4a65dbd420d9ea872f5fea77fe5ec0030a1c61
Sean Humbarger <sean.humbarger@outlook.com>
2025-12-13 11:05:41 -0700

    update doc urls

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://seanhumbarger.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/seanhumbarger/things3-workflow/',
+  baseUrl: '/things3-workflow/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
