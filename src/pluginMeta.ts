b722f9cf416c2af86c3a694eeed4f314ddad5076
Sean Humbarger <sean.humbarger@outlook.com>
2025-12-14 09:43:21 -0700

    updated product description

--- src/pluginMeta.ts
+++ src/pluginMeta.ts
@@ -5,5 +5,5 @@ export const PLUGIN_DOCS_URL = 'https://seanhumbarger.github.io/things3-workflow
 export const PLUGIN_ISSUE_URL = 'https://github.com/seanhumbarger/things3-workflow/issues';
 export const PLUGIN_BUYMECOFFEE_URL = 'https://www.buymeacoffee.com/seanhumbarger'; // <-- replace with your actual link
 export const PLUGIN_NAME = 'Things3 Workflow';
-export const PLUGIN_DESCRIPTION = 'Import your things3 tasks into obsidian notes.';
+export const PLUGIN_DESCRIPTION = 'Import your things3 tasks into your notes.';
 
